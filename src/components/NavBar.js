5affa2bf843a3605f6ea4034126dcbb4a35b4353
Sheila <rugut.cheruto@student.moringaschool.com>
2022-10-05 20:28:45 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+const linkStyles = {
+  display: "block"
 }
+function NavBar() {
+  return (
+    <div className="navbar">
+      <NavLink to="/"
+      exact
+      style={linkStyles}>Home Page  
+      </NavLink>
+      <NavLink to="/movies"
+      exact
+      style={linkStyles}
+      >Movies Page  </NavLink>
+      <NavLink to="/directors"
+      exact
+      style={linkStyles}>Directors Page  </NavLink>
+      <NavLink to="/actors"
+      exact
+      style={linkStyles}>Actors Page  </NavLink>
+    </div> 
+  );
+};
 
 export default NavBar;
